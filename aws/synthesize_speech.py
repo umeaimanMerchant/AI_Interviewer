77c6421597c9489d9cbb5e56c76dcd3c2ee834d7
Umeaiman Merchant <52196824+umeaimanMerchant@users.noreply.github.com>
2024-03-11 21:19:31 +0530

    Update synthesize_speech.py

--- aws/synthesize_speech.py
+++ aws/synthesize_speech.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import os
+import time
+import glob
+import os
 
+
+from gtts import gTTS
+from googletrans import Translator
+
+translator = Translator()
+
+def get_audio(theText="Sorry, no input"):
+    tld = "com"
+    tts = gTTS(theText, lang="en", tld=tld, slow=False)
+
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio_okay"
+    tts.save(f"{my_file_name}.mp3")
+    return my_file_name
